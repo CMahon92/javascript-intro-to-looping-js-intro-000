833645ece6bfbd00422889e45024239d2569ab13
Carleen mahon <carleenmahon92@gmail.com>
2018-08-13 15:50:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,14 +1,15 @@
 //Problem 1
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
-  if (i === 1) {
-  array.push("I am " + i + " strange loop.");
-}else{
-  array.push("I am "  + i + " strange loops.");
-}
-}
+    if (i === 1) {
+      array.push("I am " + i + " strange loop.");
+    }else{
+      array.push("I am "  + i + " strange loops.");
+    }
+  }
 return array;
 }
+
 
 //Problem 2
 function whileLoop(n) {
